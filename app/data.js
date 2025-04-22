5395430234c338b5b7dad3373fc1c0f153fba785
Leila Touati <leilat014@gmail.com>
2025-04-22 16:53:34 -0700

    added new questions to presidents

--- app/data.js
+++ app/data.js
@@ -1,107 +1,91 @@
 export const quiz = {
-    totalQuestions: 9,
-    questions: [
-      {
-        id: 1,
-        question: 'What is your top priority for San Luis Obispo?',
-        answers: {
-          A: 'Preserving open spaces and the city’s natural beauty',
-          B: 'Finding solutions to the housing and homelessness crisis',
-          C: 'Revitalizing the downtown area to support local businesses',
-          D: 'Advancing the environmental goals of the city'
-        },
+  totalQuestions: 7,
+  questions: [
+    {
+      id: 1,
+      question:
+        "How should the ASI president include diversity, equity and inclusion efforts in their leadership?",
+      answers: {
+        A: "Use past experience in DEI leadership to help students feel seen, heard and valued",
+        B: "Protect and expand scholarships for all student groups",
+        C: "Recruit a diverse cabinet that represents the student body",
+        D: "Take action to address the needs of students through creating new services and resources",
       },
-      {
-        id: 2,
-        question: 'How important is a candidate\'s experience in local government to you?',
-        answers: {
-          A: 'Very important. I prefer a candidate with a demonstrated track record of success in government',
-          B: 'Somewhat important. Experience is valuable, but a candidate’s experience in other professions is important too',
-          C: 'Not very important. I prioritize a candidates vision for the future and their proposed ideas',
-          D: 'While experience is valuable, I prioritize a candidate\'s engagement outside of government and understanding the city’s challenges and opportunities'
-        },
+    },
+    {
+      id: 2,
+      question:
+        "How should the ASI president support the student body through major transitions like the Cal Maritime integration and the semester conversion?",
+      answers: {
+        A: "Create a student advisory board to build trust between ASI and the student body",
+        B: "Hold public town hall meetings with President Armstrong",
+        C: "Ensure student fees are used appropriately and allow students to give feedback",
+        D: "Prioritize current Cal Poly students through the transition",
       },
-      {
-        id: 3,
-        question: 'Which leadership style do you find most appealing in a candidate?',
-        answers: {
-          A: 'A balanced approach that blends experience, pragmatism, and a commitment to the well-being of all residents',
-          B: 'A collaborative approach that prioritizes listening to diverse opinions and reaching consensus',
-          C: 'A style that is a blend of action-oriented, visionary thinking, and engaging the community',
-          D: 'A style that is focused on advocacy for community needs, solution-oriented, and challenging the status quo'
-        },
+    },
+    {
+      id: 3,
+      question:
+        "How should the ASI president address St. Patrick's Day celebrations?",
+      answers: {
+        A: "Increase the number of on-campus events for students year-round",
+        B: "Continue to host on-campus St. Patrick's Day celebrations",
+        C: "Protect and expand on current St. Patrick’s Day events",
+        D: "Focus on improving the relationship between Cal Poly students and the San Luis Obispo community",
       },
-      {
-        id: 4,
-        question: 'What is your stance on the role of government in addressing social issues like homelessness?',
-        answers: {
-          A: 'The government should provide a robust safety net and support services for vulnerable populations',
-          B: 'The government should play a role and continue to implement and strengthen existing programs aimed to assist those experiencing homelessness',
-          C: 'The government should play an active role in addressing homelessness, prioritizing the downtown area as a shared space for the community',
-          D: 'The government should proactively address mental health and addiction issues as they are often intertwined with homelessness'
-        },
+    },
+    {
+      id: 4,
+      question:
+        "How should ASI enhance campus events to foster student engagement and community?",
+      answers: {
+        A: "Protect and support student organizations on campus",
+        B: "Work with campus leaders to ensure events reflect what students really want",
+        C: "Support student clubs and organizations in hosting more engaging events to foster community",
+        D: "Work with the administration to create policy and events that help students find community",
       },
+    },
 
-      {
-        id: 5,
-        question: 'How important is environmental sustainability to you when considering a candidate?',
-        answers: {
-          A: 'Very important. I want a candidate who prioritizes climate action and environmental protection and creates a sustainable future for San Luis Obispo',
-          B: 'Extremely important. I support environmental initiatives, and specific targets toward climate change mitigations and reducing greenhouse gas emissions',
-          C: 'Environmental sustainability is crucial, but it should be approached with realistic, sustainable solutions that ensure we have sufficient energy resources to support our goals',
-          D: 'Not a major concern for me. I prioritize other issues facing the city'
-        },
+    {
+      id: 5,
+      question:
+        "How should ASI improve transportation and accessibility on and off campus?",
+      answers: {
+        A: "Utilize external partnerships to bring in innovative solutions",
+        B: "Prioritize student voices in local city government discussions",
+        C: "Focus on student-led feedback to inform better campus decisions",
+        D: "Expand bus routes to grocery stores and improve commuter parking affordability",
       },
+    },
 
-      {
-        id: 6,
-        question: 'How should the city address the needs of college students, specifically those attending Cal Poly?',
-        answers: {
-          A: 'Prioritize affordable housing options and tenant protections to ensure students have access to safe and affordable housing',
-          B: 'Focus on student safety, particularly in relation to traffic and transportation infrastructure, to prevent accidents and ensure safe commutes',
-          C: 'Prioritize revitalizing the downtown area, creating spaces that cater to student interests and provide social engagement opportunities',
-          D: 'Focus on downtown businesses that college students work at, addressing homelessness in the area that negatively impacts them'
-        },
+    {
+      id: 6,
+      question:
+        "How should the ASI President support student organizations and club funding?",
+      answers: {
+        A: "Advocate for external partnerships that can help sponsor student clubs",
+        B: "Reform funding policy to ensure transparency",
+        C: "Make student fees work more directly for clubs and ensure clubs have the resources to thrive",
+        D: "Increase club funding and expand access to on-campus spaces for club meetings and events",
       },
+    },
 
-      {
-        id: 7,
-        question: 'How important is it to you that a candidate actively advocates for the Cal Poly community?',
-        answers: {
-          A: 'Very important. I want a candidate who understands the needs of students and faculty and will represent their interests',
-          B: 'It is important for a candidate to recognize and value Cal Poly\'s significant impact on the city and work towards fostering positive relationships between the university and the wider community',
-          C: 'Somewhat important. It is beneficial for the candidate to be connected to the university, but it should not be their sole focus',
-          D: 'Not a major factor for me. I am more concerned with their broader vision for the city'
-        },
+    {
+      id: 7,
+      question:
+        "How should ASI build trust and accountability with the student body?",
+      answers: {
+        A: "Use past leadership roles to personally connect with students and advocate on their behalf",
+        B: "Host regular listening sessions and follow-up on student feedback",
+        C: "Launch campaigns and systems for students to easily share concerns with ASI and get answers",
+        D: "Ensure all student concerns are addressed through visible actions and results",
       },
-
-      {
-        id: 8,
-        question: 'What is your opinion on the current tone of political discourse, particularly as it relates to local elections?',
-        answers: {
-          A: 'While I value respectful dialogue, I believe it is important for candidates to be honest and transparent in their communication, even when addressing difficult or controversial issues',
-          B: 'It is often too divisive and unproductive. I favor a candidate who promotes civility and respectful dialogue, even when disagreements arise',
-          C: 'I am not overly concerned with the tone of political discourse, as long as the candidate is effective in achieving positive outcomes for the city',
-          D: 'I value passionate advocacy, even if it involves expressing strong opinions that may challenge others'
-        },
-      },
-
-      {
-        id: 9,
-        question: 'What is your preferred approach to city budgeting?',
-        answers: {
-          A: 'Prioritize fiscal responsibility, limit spending, and avoid raising taxes as much as possible',
-          B: 'Believing in fiscal responsibility and using their experience in city budgeting to invest in city planning',
-          C: 'No approach was stated',
-          D: 'No approach necessary'
-        },
-      },
-      
-    ],
-    results: {
-      A: 'Jan Marx. Marx is running for re-election for SLO City Council. Marx\'s main focus is on preserving the city\'s open spaces and supporting renters.',
-      B: 'Michael Boswell. Boswell is running for SLO City Council, and is a city and regional planning professor at Cal Poly. His focus is on climate change and addressing housing challenges.',
-      C: 'John Drake. Drake is a Cal Poly alum and is passionate about revitlaizing the downtown area and energy infrastructure in the city.',
-      D: 'Felicia Lewis. Lewis is the owner of Gumbo Girl SLO, and is focused on supporting local businesses.',
-    }
-  };
+    },
+  ],
+  results: {
+    A: "Alexis Espinoza",
+    B: "Reagan Denny",
+    C: "Colin Shea",
+    D: "Marc Cabeliza",
+  },
+};
